91a89b6e48695856fa6b1dced490a89f92c07fd8
Yasmeen Nasr <yasmeenagag@gmail.com>
2022-12-13 22:43:53 +0200

    inital commit

---
+++ irrigation-system/src/main/java/com/banque/misr/irrigation/system/irrigationsystem/IrrigationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.banque.misr.irrigation.system.irrigationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrrigationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IrrigationSystemApplication.class, args);
+    }
+
+}
